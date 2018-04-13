46288920a015d9743219b7313add2cfb29a5a7d6
Vladimir <twnk@list.ru>
2018-04-13 06:26:16 +0500

    Added cycle.

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
     parser.add_argument('--scale', default=1.5, type=int)
     scale = parser.parse_args().scale
     songs = [smoke, lambada, army, popcorn, yesterday, bells, tatu, march]
-    index = 0
-    while index < len(songs):
-        difficulty = Difficulty(songs[index].tempo + index * 100 // 4, index // 2)
-        game = Game(songs[index], difficulty)
-        if GUI(game, scale).mainloop():
-            index += 1
+    while True:
+        index = 0
+        while index < len(songs):
+            difficulty = Difficulty(songs[index].tempo + index * 100 // 4, index // 2)
+            game = Game(songs[index], difficulty)
+            if GUI(game, scale).mainloop():
+                index += 1
 
-    GUI(FreeGame(), scale).mainloop()
+        GUI(FreeGame(), scale).mainloop()
